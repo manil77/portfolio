1f691653c4dbb60c55e1857e4c9fc1f0cbb9040d
Manil Maharjan <manil@inficare.com.my>
2024-12-05 07:36:46 +0545

    Setting stage for Authentication and Authorisation

---
+++ Portfolio/Controllers/AuthController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio.Controllers
+{
+    public class AuthController : Controller
+    {
+        //[Route("login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
