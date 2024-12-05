641ce1ada008cec4b609b098144a03bad3a3e2bd
manil77 <manil@inficare.com.my>
2024-12-05 14:34:49 +0545

    Added API Project

---
+++ API/PortfolioController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PortfolioController: ControllerBase
+    {
+        [HttpGet]
+        [Route("api/test")]
+        public string Test() 
+        {
+            return "Test";
+        }
+
+    }
+}
